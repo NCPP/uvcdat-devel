44214d3fbb43eb94e7bd6a296b2417d6fa7fa3a9
Charles Doutriaux <doutriaux1@llnl.gov>
2013-10-10 10:22:22 -0700

    Trying to get tk built for mac

--- CMake/cdat_modules/tcltk_external.cmake
+++ CMake/cdat_modules/tcltk_external.cmake
@@ -3,11 +3,21 @@ set(tcl_source "${CMAKE_CURRENT_BINARY_DIR}/build/tcl")
 set(tk_source "${CMAKE_CURRENT_BINARY_DIR}/build/tk")
 set(tcltk_install "${cdat_EXTERNALS}")
 
-set(tcltk_configure_args --enable-shared)
 
 # tcl
 #
 set(proj tcl-${TCLTK_MAJOR}.${TCLTK_MINOR})
+
+# Build dir is different for apple
+if (APPLE)
+    set(SUBTKDIR macosx)
+    set(tcltk_configure_args --enable-aqua)
+    set(ENV_ARGS "env PREFIX=${cdat_EXTERNALS} INSTALL_PATH=${cdat_EXTERNALS}/Library/Frameworks")
+else()
+    set(SUBTKDIR unix)
+    set(tcltk_configure_args --enable-shared)
+endif()
+message([INFO] SUBTKDIR IS: ${SUBTKDIR})
 
 ExternalProject_Add(${proj}
   DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
@@ -16,9 +26,10 @@ ExternalProject_Add(${proj}
   URL ${TCLTK_URL}/${TCL_GZ}
   URL_MD5 ${TCL_MD5}
   BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR>/unix -DCONFIGURE_ARGS=${tcltk_configure_args} -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
-  BUILD_COMMAND ${CMAKE_COMMAND} -Dmake=$(MAKE) -DWORKING_DIR=<SOURCE_DIR>/unix -P ${cdat_CMAKE_BINARY_DIR}/cdat_make_step.cmake
-  INSTALL_COMMAND ${CMAKE_COMMAND} -DWORKING_DIR=<SOURCE_DIR>/unix -P ${cdat_CMAKE_BINARY_DIR}/cdat_install_step.cmake
+  WORKING_DIR ${tcl_source}/macosx
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR>/${SUBTKDIR} -DENV_ARGS=${ENV_ARGS} -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  BUILD_COMMAND ${CMAKE_COMMAND} -DENV_ARGS=${ENV_ARGS} -Dmake=$(MAKE) -DWORKING_DIR=<SOURCE_DIR>/${SUBTKDIR} -P ${cdat_CMAKE_BINARY_DIR}/cdat_make_step.cmake
+  INSTALL_COMMAND ${CMAKE_COMMAND} -DENV_ARGS=${ENV_ARGS} -DWORKING_DIR=<SOURCE_DIR>/${SUBTKDIR} -P ${cdat_CMAKE_BINARY_DIR}/cdat_install_step.cmake
   DEPENDS ${TclTk_deps}
   ${ep_log_options}
 )
@@ -34,9 +45,9 @@ ExternalProject_Add(${proj}
   URL ${TCLTK_URL}/${TK_GZ}
   URL_MD5 ${TK_MD5}
   BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR>/unix -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
-  BUILD_COMMAND ${CMAKE_COMMAND} -Dmake=$(MAKE) -DWORKING_DIR=<SOURCE_DIR>/unix -P ${cdat_CMAKE_BINARY_DIR}/cdat_make_step.cmake
-  INSTALL_COMMAND ${CMAKE_COMMAND} -DWORKING_DIR=<SOURCE_DIR>/unix -P ${cdat_CMAKE_BINARY_DIR}/cdat_install_step.cmake
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR>/${SUBTKDIR} -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  BUILD_COMMAND ${CMAKE_COMMAND} -Dmake=$(MAKE) -DWORKING_DIR=<SOURCE_DIR>/${SUBTKDIR} -P ${cdat_CMAKE_BINARY_DIR}/cdat_make_step.cmake
+  INSTALL_COMMAND ${CMAKE_COMMAND} -DWORKING_DIR=<SOURCE_DIR>/${SUBTKDIR} -P ${cdat_CMAKE_BINARY_DIR}/cdat_install_step.cmake
   DEPENDS tcl-${TCLTK_MAJOR}.${TCLTK_MINOR}
   ${ep_log_options}
 )
